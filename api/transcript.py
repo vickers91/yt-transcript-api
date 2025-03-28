23c46c03eaa64d9aca25c26b4efb608851542352
vickers91 <julio.idb@gmail.com>
2025-03-28 10:06:54 -0300

    Update transcript.py

--- api/transcript.py
+++ api/transcript.py
@@ -2,20 +2,11 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from urllib.parse import urlparse, parse_qs
 import json
 
-def handler(request):
+def handler(event, context):
     try:
-        if request.method == "GET":
-            video_url = request.args.get("url")
-        elif request.method == "POST":
-            body = request.get_json()
-            video_url = body.get("url")
-        else:
-            return {
-                "statusCode": 405,
-                "body": json.dumps({"error": "Method not allowed"})
-            }
+        query = event.get("queryStringParameters", {})
+        video_url = query.get("url", "")
 
-        # Extrair o ID do vídeo
         parsed_url = urlparse(video_url)
         video_id = parse_qs(parsed_url.query).get("v")
 
@@ -26,8 +17,8 @@ def handler(request):
             }
 
         video_id = video_id[0]
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['pt', 'en'])
-        full_text = "\n".join([entry['text'] for entry in transcript])
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=["pt", "en"])
+        full_text = "\n".join([entry["text"] for entry in transcript])
 
         return {
             "statusCode": 200,
@@ -40,4 +31,3 @@ def handler(request):
             "statusCode": 500,
             "body": json.dumps({"error": str(e)})
         }
-
